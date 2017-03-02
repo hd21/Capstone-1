30b98272b51adc58db03c62ffaca3ad6ac57b9d0
hd21 <hazelmarie.doctor@gmail.com>
2017-03-01 22:28:58 -0500

    Added new elements in HTML, header styles in CSS

--- app.js
+++ app.js
@@ -1,62 +1,80 @@
-$(document).ready(function() {
+$(document)
+    .ready(function() {
 
-    function retrieveDataFromApi(searchTerm, callback) {
+        function retrieveDataFromApi(searchTerm, callback) {
 
-        var settings = {
+            var settings = {
 
-            url: 'https://www.googleapis.com/youtube/v3/search',
-            data: {
+                url: 'https://www.googleapis.com/youtube/v3/search',
+                data: {
 
-                part: 'snippet',
-                key: 'AIzaSyASovBdr9Ul8NmoI2nbudX1sv91kxupWhY',
-                q: searchTerm,
-                r: 'json',
-                maxResults: 3
+                    part: 'snippet',
+                    key: 'AIzaSyASovBdr9Ul8NmoI2nbudX1sv91kxupWhY',
+                    q: searchTerm,
+                    r: 'json',
+                    maxResults: 3
 
-            },
+                },
 
-            dataType: 'json',
-            type: 'GET',
-            success: callback
-        };
+                dataType: 'json',
+                type: 'GET',
+                success: callback
+            };
 
-        $.ajax(settings);
-    }
+            $.ajax(settings);
+        }
 
-    function showYoutubeSearchData(data) {
+        function showYoutubeSearchData(data) {
 
-        var html = data.items.map(function(item) {
+            var html = data
+                .items
+                .map(function(item) {
 
-            var $template = $('<div class="left-div"><img class="img-responsive thumbnail"><div class="info-group"><h4 class="space"><a class="video-id"></a></h4><small>By<span class="channel-title"></span> </small><p><a class="channel-id"><button type="button">View Channel</button></a></p><p class="description"></p></div></div>');
+                    var $template = $('<div class="left-div"><img class="img-responsive thumbnail"><div class="info-gro' +
+                        'up"><h4 class="space"><a class="video-id"></a></h4><small>By<span class="channel' +
+                        '-title"></span> </small><p><a class="channel-id"><button type="button">View Chan' +
+                        'nel</button></a></p><p class="description"></p></div></div>');
 
-            var vidID = item.id.videoId;
-            var title = item.snippet.title;
-            var description = item.snippet.description;
-            var thumbnail = item.snippet.thumbnails.high.url;
-            var channelTitle = item.snippet.channelTitle;
-            var channelID = item.snippet.channelId;
+                    var vidID = item.id.videoId;
+                    var title = item.snippet.title;
+                    var description = item.snippet.description;
+                    var thumbnail = item.snippet.thumbnails.high.url;
+                    var channelTitle = item.snippet.channelTitle;
+                    var channelID = item.snippet.channelId;
 
-            $template.find('.video-id').attr('href', 'http://youtube.com/embed/' + vidID + '?rel=0').text(title);
-            $template.find('.thumbnail').attr('src', thumbnail);
-            $template.find('.channel-id').attr('href', 'https://www.youtube.com/channel/' + channelID);
-            $template.find('.description').text(description);
-            $template.find('.channel-title').text(channelTitle);
+                    $template
+                        .find('.video-id')
+                        .attr('href', 'http://youtube.com/embed/' + vidID + '?rel=0')
+                        .text(title);
+                    $template
+                        .find('.thumbnail')
+                        .attr('src', thumbnail);
+                    $template
+                        .find('.channel-id')
+                        .attr('href', 'https://www.youtube.com/channel/' + channelID);
+                    $template
+                        .find('.description')
+                        .text(description);
+                    $template
+                        .find('.channel-title')
+                        .text(channelTitle);
 
-            return $template;
+                    return $template;
 
-        });
+                });
 
-        $('#meetup-column').removeClass("hidden");
-        $('.js-yt-search-results').html(html);
+            $('#meetup-column').removeClass("hidden");
+            $('.js-yt-search-results').html(html);
 
-    }
+        }
 
-    $('#exercise-list').change(function() {
-        var selected = $(this).val() + " Exercises";
-        retrieveDataFromApi(selected, showYoutubeSearchData);
+        $('#exercise-list')
+            .change(function() {
+                var selected = $(this).val() + " Exercises";
+                retrieveDataFromApi(selected, showYoutubeSearchData);
+            });
+
     });
-
-});
 
 function getMeetUpData(search, location) {
 
@@ -89,15 +107,9 @@ function showMeetUpSearchData(data) {
         eventInElement += '<p> No results </p>';
     } else {
 
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < 3; i++) {
 
-            eventInElement += "<li>";
-            eventInElement += "<p>" + data[i].name + "</p>";
-            eventInElement += "<p class = 'meetup-description'>" + data[i].description + "</p>";
-            eventInElement += "<p class = 'meetup-state'>" + "State:" + " " + data[i].state + "</p>";
-            eventInElement += "<p class = 'meetup-state'>" + "City:" + " " + data[i].city + "</p>";
-            eventInElement += "<a class = 'meetup-link' href = " + "'" + data[i].link + "'" + ">" + "Event Page" + "</a><br>";
-            eventInElement += "</li>";
+            eventInElement += '<div class="right-div"><p>Event:<br>' + data[i].name + '</p><p>Location:<br>' + data[i].city + ', ' + data[i].state + '</p><p>Description:<br>' + data[i].description + '</p><a href ="' + data[i].link + '">Event Page</a><br></div>';
 
         }
 
@@ -105,17 +117,19 @@ function showMeetUpSearchData(data) {
     }
 }
 
+$('#location-input')
+    .submit(function(event) {
+        event.preventDefault();
 
-$('#location-input').submit(function(event) {
-    event.preventDefault();
+        var searchEvent = $("#exercise-list")
+            .find(":selected")
+            .text();
+        var locationEvent = $(".address").val();
 
-    var searchEvent = $("#exercise-list").find(":selected").text();
-    var locationEvent = $(".address").val();
+        if (searchEvent.length && locationEvent.length || event.key === 'Enter') {
+            getMeetUpData(searchEvent, locationEvent, showMeetUpSearchData);
+        } else {
+            alert('Please select an exercise type and enter your current location.');
+        }
 
-    if (searchEvent.length && locationEvent.length || event.key === 'Enter') {
-        getMeetUpData(searchEvent, locationEvent, showMeetUpSearchData);
-    } else {
-        alert('Please input a city and exercise type!');
-    }
-
-});
+    });
